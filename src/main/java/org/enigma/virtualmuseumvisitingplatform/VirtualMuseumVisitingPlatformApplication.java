0c7c0451c09e9dfc8a97db00d196a955e9bda71a
abilov042 <shamilabilov599@gmail.com>
2024-08-11 21:48:18 +0400

    first commit

---
+++ src/main/java/org/enigma/virtualmuseumvisitingplatform/VirtualMuseumVisitingPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.enigma.virtualmuseumvisitingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualMuseumVisitingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualMuseumVisitingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/enigma/virtualmuseumvisitingplatform/VirtualMuseumVisitingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enigma.virtualmuseumvisitingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualMuseumVisitingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
